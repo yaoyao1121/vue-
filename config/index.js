b80487c484df41734c92306cdbc8b3db27a140a5
yaoyao1121 <33979626+yaoyao1121@users.noreply.github.com>
2018-04-11 15:23:09 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -15,7 +15,7 @@ module.exports = {
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    autoOpenBrowser: false,  //是否自动打开默认浏览器  false不打开   true 打开
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
