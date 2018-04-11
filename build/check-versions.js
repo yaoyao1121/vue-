18b7afa007c52f360c75aebcb4f75b1f8b350eff
yaoyao1121 <33979626+yaoyao1121@users.noreply.github.com>
2018-04-11 16:28:41 +0800

    Update check-versions.js

--- build/check-versions.js
+++ build/check-versions.js
@@ -1,30 +1,40 @@
 // 对版本进行校验
 'use strict'
+//chalk是一个用来再命令行输出不同颜色文字的包，可以使用chalk.yellow("想添加颜色的文字......")
+//来实现改变文字颜色的
 const chalk = require('chalk')
+//semver 的是一个语义化版本文件的npm包，其实它就是用来控制版本的
 const semver = require('semver')
 const packageConfig = require('../package.json')
+//一个用来执行unix命令的包
 const shell = require('shelljs')
-
+//child_process 是Node.js提供了衍生子进程功能的模块，execSync()方法同步执行一个cmd命令，
+//将返回值的调用toString和trim方法
 function exec (cmd) {
-  return require('child_process').execSync(cmd).toString().trim()
+  return require('child_process').execSync(cmd).toString().trim() //转换成字符串并去掉空格
 }
 
 const versionRequirements = [
   {
     name: 'node',
+    //semver.clean()方法返回一个标准的版本号，去掉两边的空格，比如semver.clean(" =v1.2.3 ")
+    //返回"1.2.3"，此外semver还有vaild,satisfies,gt,lt等方法
+    //这里查看https://npm.taobao.org/package/semver可以看到更多关于semver方法的内容
     currentVersion: semver.clean(process.version),
     versionRequirement: packageConfig.engines.node
   }
 ]
-
+//shell.which方法是去环境变量搜索有没有参数这个命令
 if (shell.which('npm')) {
   versionRequirements.push({
     name: 'npm',
+    //执行"npm --version"命令
     currentVersion: exec('npm --version'),
     versionRequirement: packageConfig.engines.npm
   })
 }
 
+//抛出错误函数
 module.exports = function () {
   const warnings = []
 
